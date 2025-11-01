b9bc69f1cc54603fc3afc8c9c7140561a411af47
fakhrihabb <fakhrihabb@gmail.com>
2025-11-01 16:05:22 +0700

    chore: added image whitelist

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,18 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "gkmoljficwdyeqnyvfpn.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.cdnlogo.com",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
     ],
   },
 };
